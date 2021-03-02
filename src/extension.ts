999cc416d7a50689a31780a7f8f50a20618fe138
HelloCore <aukwat@gmail.com>
2021-03-02 15:01:34 +0700

    Implement Util

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,85 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { debug } from "console";
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const dartExt = vscode.extensions.getExtension("Dart-Code.dart-code");
+  if (dartExt == null) {
+    vscode.window.showErrorMessage(
+      "Cannot initialize fluttercodeutil, make sure you has already installed dart extension"
+    );
+    return;
+  }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fluttercodeutil" is now active!');
+  const { exports } = dartExt;
+  if (exports == null) {
+    vscode.window.showErrorMessage(
+      "Cannot initialize fluttercodeutil, make sure you has already installed dart extension"
+    );
+    return;
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fluttercodeutil.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  const { debugCommands, debugProvider } = exports._privateApi;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from FlutterCodeUtil!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "fluttercodeutil.printCurrentDevice",
+      () => {
+        if (debugProvider?.deviceManager?.currentDevice != null) {
+          return JSON.stringify(debugProvider?.deviceManager?.currentDevice);
+        }
+        vscode.commands.executeCommand("flutter.selectDevice");
+        vscode.window.showErrorMessage("Please select device first.");
+        return null;
+      }
+    )
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "fluttercodeutil.getCurrentAppBinary",
+      async () => {
+        var device = debugProvider?.deviceManager?.currentDevice;
+        if (device == null) {
+          device = await vscode.commands.executeCommand("flutter.selectDevice");
+        }
+        if (device == null) {
+          vscode.window.showErrorMessage("Please select device first.");
+          return null;
+        }
+        if (debugProvider?.deviceManager?.currentDevice != null) {
+          const config = vscode.workspace.getConfiguration("fluttercodeutil");
+          const { currentDevice } = debugProvider?.deviceManager;
+          if (currentDevice.platformType == "ios") {
+            if (currentDevice.emulator) {
+              return config.get<string>("iphonesimulatorAppPath");
+            } else {
+              return config.get<string>("iphoneosAppPath");
+            }
+          }
+          return config.get<string>("androidAppPath");
+        }
+      }
+    )
+  );
+
+  const onDebugSessionVmServiceAvailable: vscode.Event<any> =
+    debugCommands.onDebugSessionVmServiceAvailable;
+  context.subscriptions.push(
+    onDebugSessionVmServiceAvailable((event) => {
+      console.log({ event });
+      const config = vscode.workspace.getConfiguration("fluttercodeutil");
+      const hotRestartAfterLaunch = config.get<boolean>(
+        "hotRestartAfterLaunch"
+      );
+      if (hotRestartAfterLaunch == true) {
+        setTimeout(() => {
+          vscode.commands.executeCommand("flutter.hotRestart");
+        }, 2000);
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
